32ae51fb1589060a94d9c9a51840e880a2f4e03b
abbassher94 <57453785+Abbassher55@users.noreply.github.com>
2024-09-21 13:05:31 +0500

    Project changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const tailwindRtl = require("tailwindcss-rtl");
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: {
-          lg: "4rem",
-          md: "3rem",
-        },
-      },
-      fontFamily: {
-        tawajal: ["Tawajal", "sans-serif"],
-      },
-      colors: {
-        "menu-blue": "#1B4166",
-        "half-blue": "#799FC3",
-        "theme-blue": "#094C70",
-        "theme-yellow": "#FFC000",
-        "theme-green": "#03625F",
-        "theme-light-white": "#EEF5FF",
-        "theme-cream": "#F1EEE9",
-        "section-light-blue": "#c5dff84d",
-        "title-light": "#91c8e433",
-        "footer-input": "#CACCCF",
-      },
-    },
-    variants: {
-      extend: {
-        backgroundImage: ["before"],
-      },
-    },
+    extend: {},
   },
-  plugins: [tailwindRtl],
+  plugins: [],
 };
